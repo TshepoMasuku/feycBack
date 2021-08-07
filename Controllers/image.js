8b9d587400d5061e50868222444ff4b611584d9b
Tshepo Masuku <tshepomasuku16@gmail.com>
2021-08-07 17:27:16 +0200

    heroku bullshit

--- Controllers/image.js
+++ Controllers/image.js
@@ -5,6 +5,15 @@ const app = new Clarifai.App({
     apiKey: '3176a6426124469aaae986f2d621c227' 
 });
 
+const handleAPIcall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input) 
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API.'))
+}
+
 const handleImage = (db, req, res) => {
     const { id } = req.body;
     db("users").where("id","=",id)
@@ -13,16 +22,6 @@ const handleImage = (db, req, res) => {
         .then( entries => res.json(entries[0]) )
         .catch(err => res.status(404).json("Error User Entries Not Found.", err))
 };
-
-const handleAPIcall = (req, res) => {
-    app.models
-        // .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-        .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => res.status(400).json('unable to work with API'))
-}
 
 module.exports = {
     handleImage,
