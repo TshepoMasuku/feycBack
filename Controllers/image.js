264452c2349f73b6546248037a210900b64172e3
TshepoMasuku <tshepomasuku16@gmail.com>
2023-01-16 16:48:52 +0200

    fixing Clarifai API configurations in image.js

--- Controllers/image.js
+++ Controllers/image.js
@@ -6,8 +6,14 @@ const app = new Clarifai.App({
 });
 
 const handleAPIcall = (req, res) => {
-    app.models
-        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input) 
+    // app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input) 
+    app.models.predict(
+        {
+            id: 'face-detection',
+            name: 'face-detection',
+            version: '6dc7e46bc9124c5c8824be4822abe105',
+            type: 'visual-detector',
+        }, req.body.input) 
         .then(data => res.json(data))
         .catch(err => res.status(400).json('unable to work with API.'))
 }
